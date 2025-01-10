6cdec593ddae4f57fa7cba34e6f44de5e1809c5b
Rob Welch <sparksoftware@googlemail.com>
2025-01-10 02:17:20 +0000

    Updates for relion, fixed formatting

--- sample_plots.sh
+++ sample_plots.sh
@@ -5,8 +5,68 @@
 
 # QM
 # ARCHER2
-hpcbench bars -m meta:QM=Psi4 -x meta:basis -y Accounting:ElapsedRaw -l meta:Machine -d ~/benchout -o bars_test.pdf
+hpcbench bars \
+-m meta:QM=Psi4 \
+-m meta:method=ccsd \
+-x meta:set \
+-y Accounting:ElapsedRaw \
+-a def2-tzvpp:BEDE-GH=Segfault \
+-a def2-tzvppd:ISAMBARD=Segfault \
+-l meta:Machine \
+-d ~/benchout \
+--xaxislabel "'Basis set'" \
+--yaxislabel "'Benchmark suite runtime (s)'" \
+-o bars_test.pdf
+
 hpcbench bars -m meta:QM=Psi4 -x meta:basis -y Accounting:AveRSS -l meta:Machine -d ~/benchout -o bars_test_mem.pdf
+
+# QM scaling
+
+hpcbench scaling \
+--matching "'meta:Machine=Archer2'" \
+--matching "'meta:QM=Psi4'" \
+--matching "'meta:method=ccsd'" \
+--x "'meta:Memory'" \
+--y "'Accounting:ElapsedRaw'" \
+--l "'meta:set'" \
+--directory "'/home/rob/benchout/archer/psi4'" \
+--outside \
+--xaxislabel "'Memory (GB)'" \
+--yaxislabel "'Benchmark suite runtime (s)'" \
+--outfile archer2_qm_ccsd.pdf
+
+hpcbench scaling \
+--matching "'meta:Machine=Archer2'" \
+--matching "'meta:QM=Psi4'" \
+--matching "'meta:method=scf'" \
+--x "'meta:Memory'" \
+--y "'Accounting:ElapsedRaw'" \
+--l "'meta:set'" \
+--directory "'/home/rob/benchout/archer/psi4'" \
+--outside \
+--outfile archer2_qm_scf.pdf
+
+hpcbench scaling \
+--matching "'meta:Machine=Archer2'" \
+--matching "'meta:QM=Psi4'" \
+--matching "'meta:method=mp2'" \
+--x "'meta:Memory'" \
+--y "'Accounting:ElapsedRaw'" \
+--l "'meta:set'" \
+--directory "'/home/rob/benchout/archer/psi4'" \
+--outside \
+--outfile archer2_qm_mp2.pdf
+
+hpcbench scaling \
+--matching "'meta:Machine=Archer2'" \
+--matching "'meta:QM=Psi4'" \
+--matching "'meta:method=hf'" \
+--x "'meta:Memory'" \
+--y "'Accounting:ElapsedRaw'" \
+--l "'meta:set'" \
+--directory "'/home/rob/benchout/archer/psi4'" \
+--outside \
+--outfile archer2_qm_hf.pdf
 
 # MD
 # Scaling system size on JADE
@@ -73,3 +133,206 @@ hpcbench scaling \
 --outside \
 --outfile gh_power.pdf
 
+
+# best threads
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=GROMACS'" \
+--matching "'run:Totals:Number of atoms=2997924.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile gromacs_optimal_threads_3000k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=GROMACS'" \
+--matching "'run:Totals:Number of atoms=1403180.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile gromacs_optimal_threads_1400k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=GROMACS'" \
+--matching "'run:Totals:Number of atoms=465399.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile gromacs_optimal_threads_465k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=GROMACS'" \
+--matching "'run:Totals:Number of atoms=61153.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile gromacs_optimal_threads_61k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=GROMACS'" \
+--matching "'run:Totals:Number of atoms=19605.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile gromacs_optimal_threads_20k.pdf
+
+
+
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=namd2'" \
+--matching "'run:Totals:Number of atoms=2997924.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile namd_optimal_threads_3000k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=namd2'" \
+--matching "'run:Totals:Number of atoms=1403180.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile namd_optimal_threads_1400k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=namd2'" \
+--matching "'run:Totals:Number of atoms=465399.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile namd_optimal_threads_465k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=namd2'" \
+--matching "'run:Totals:Number of atoms=61153.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile namd_optimal_threads_61k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=LAMMPS'" \
+--matching "'run:Totals:Number of atoms=19605.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile namd_optimal_threads_20k.pdf
+
+
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=LAMMPS'" \
+--matching "'run:Totals:Number of atoms=2997924.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile lammps_optimal_threads_3000k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=LAMMPS'" \
+--matching "'run:Totals:Number of atoms=1403180.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile lammps_optimal_threads_1400k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=LAMMPS'" \
+--matching "'run:Totals:Number of atoms=465399.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile lammps_optimal_threads_465k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=LAMMPS'" \
+--matching "'run:Totals:Number of atoms=61153.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile lammps_optimal_threads_61k.pdf \
+
+hpcbench scaling \
+--matching "'meta:Machine=ARCHER2'" \
+--matching "'slurm:program=LAMMPS'" \
+--matching "'run:Totals:Number of atoms=19605.0'" \
+--x "'slurm:slurm:nodes'" \
+--y "'run:Totals:ns/day'" \
+--l "'slurm:slurm:cpus-per-task'" \
+--directory "'/home/rob/benchout'" \
+--outside \
+--outfile lammps_optimal_threads_20k.pdf
+
+hpcbench bars \
+-m meta:Memorycomparison=yes \
+-x meta:Atoms \
+-y accounting:MaxRss \
+-l slurm:program \
+-d ~/benchout \
+--xaxislabel "'Number of atoms'" \
+--yaxislabel "'Memory usage (KB)'" \
+-o md_archer_memory.pdf
+
+hpcbench bars \
+-m meta:GPUMemComparison=yes \
+-x meta:Atoms \
+-y meta:GPUMem \
+-l slurm:program \
+-d ~/benchout \
+--xaxislabel "'Number of atoms'" \
+--yaxislabel "'GPU memory usage (KB)'" \
+-o md_jade2_gpu_memory.pdf
+
+# conservation of energy
+
+hpcbench log -x thermo:Etot -y thermo:Etot -l meta:Atoms -d /home/rob/benchout/lumi/ --outside --matching meta:MD=AMBER -t -o amber_lumi_energy.pdf
+
+hpcbench log -x "'thermo:Total Energy'" -y "'thermo:Total Energy'" -l meta:Atoms -d /home/rob/benchout/lumi/ --outside --matching meta:MD=Gromacs -t -o gromacs_lumi_energy.pdf
+
+hpcbench log -x "'thermo:TOTAL'" -y "'thermo:TOTAL'" -l meta:Atoms -d /home/rob/benchout/lumi/ --outside --matching meta:MD=NAMD -t -o namd_lumi_energy.pdf
+
+hpcbench log -x "'thermo:thermo:?:etotal'" -y "'thermo:thermo:?:etotal'" -l meta:Atoms -d /home/rob/benchout/lumi/ --outside --matching meta:MD=LAMMPS -t -o lmp_lumi_energy.pdf
+
+hpcbench log -x "'thermo:Conserved En.'" -y "'thermo:Conserved En.'" -l meta:Atoms -d /home/rob/benchout/lumi/ --outside --matching meta:MD=Gromacs -t -o gromacs_lumi_conserved_energy.pdf
